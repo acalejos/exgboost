1b5c707ae559b490a50c3c34fc769fa4974a8a20
acalejos <andres.alejos@gmail.com>
2023-04-23 20:12:26 -0400

    work on dmatrix bindings

--- c/exgboost/src/dmatrix.c
+++ c/exgboost/src/dmatrix.c
@@ -86,6 +86,7 @@ END:
 
 ERL_NIF_TERM EXGDMatrixCreateFromCSR(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
+    int result = -1;
     char *indptr = NULL;
     char *indices = NULL;
     char *data = NULL;
@@ -97,6 +98,102 @@ ERL_NIF_TERM EXGDMatrixCreateFromCSR(ErlNifEnv* env, int argc, const ERL_NIF_TER
         ret = error(env, "Wrong number of arguments");
         goto END;
     }
+    if (!get_string(env, argv[0], &indptr)) {
+        ret = error(env, "Indptr must be a string");
+        goto END;
+    }
+    if (!get_string(env, argv[1], &indices)) {
+        ret = error(env, "Indices must be a string");
+        goto END;
+    }
+    if (!get_string(env, argv[2], &data)) {
+        ret = error(env, "Data must be a string");
+        goto END;
+    }
+    if (!enif_get_int(env, argv[3], &ncol)) {
+        ret = error(env, "Ncol must be an integer");
+        goto END;
+    }
+    if (!get_string(env, argv[4], &config)) {
+        ret = error(env, "Config must be a string");
+        goto END;
+    }
+    printf("indptr: %s\n", indptr);
+    printf("indices: %s\n", indices);
+    printf("data: %s\n", data);
+    printf("ncol: %d\n", ncol);
+    printf("config: %s\n", config);
+    result = XGDMatrixCreateFromCSR(indptr, indices, data, ncol, config, &handle);
+    if (result == 0) {
+        ret = ok(env, enif_make_resource(env, handle));
+        enif_release_resource(handle);
+    } else {
+        ret = error(env, XGBGetLastError());
+    }
 END:
     return ret;
+}
+
+ERL_NIF_TERM EXGDMatrixCreateFromCSREx(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    ErlNifBinary indptr_bin;
+    ErlNifBinary indices_bin;
+    ErlNifBinary data_bin;
+    int result = -1;
+    uint64_t *indptr = NULL;
+    uint32_t *indices = NULL;
+    float *data = NULL;
+    uint32_t nindptr = 0;
+    uint64_t nelem = 0;
+    uint64_t ncol = 0;
+    DMatrixHandle handle;
+    ERL_NIF_TERM ret = 0;
+    if (argc != 6) {
+        ret = error(env, "Wrong number of arguments");
+        goto END;
+    }
+    if (!enif_inspect_binary(env, argv[0], &indptr_bin)) {
+        ret = error(env, "Indptr must be a binary of uint64_t");
+        goto END;
+    }
+    if (!enif_inspect_binary(env, argv[1], &indices_bin)) {
+        ret = error(env, "Indices must be a binary of uint64_t");
+        goto END;
+    }
+    if (!enif_inspect_binary(env, argv[2], &data_bin)) {
+        ret = error(env, "Data must be a binary of uint64_t");
+        goto END;
+    }
+    if (!enif_get_uint(env, argv[3], &nindptr)) {
+        ret = error(env, "Nindptr must be a uint64_t");
+        goto END;
+    }
+    if (!enif_get_uint64(env, argv[4], &nelem)) {
+        ret = error(env, "Nelem must be a uint64_t");
+        goto END;
+    }
+    if (!enif_get_uint64(env, argv[5], &ncol)) {
+        ret = error(env, "Ncol must be a uint64_t");
+        goto END;
+    }
+    indptr = (uint64_t *)indptr_bin.data;
+    indices = (uint32_t *)indices_bin.data;
+    data = (float *)data_bin.data;
+    if (indptr_bin.size != nindptr * sizeof(uint64_t)) {
+        ret = error(env, "Indptr size does not match nindptr");
+        goto END;
+    }
+    if (data_bin.size != nelem * sizeof(float)) {
+        ret = error(env, "Data size does not match nelem");
+        goto END;
+    }
+    result = XGDMatrixCreateFromCSREx(indptr, indices, data, nindptr, nelem, ncol, &handle);
+    if (result == 0) {
+        ret = ok(env, enif_make_resource(env, handle));
+        enif_release_resource(handle);
+    } else {
+        ret = error(env, XGBGetLastError());
+    }
+END:
+    return ret b;
 }
