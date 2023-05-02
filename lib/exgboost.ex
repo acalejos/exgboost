4ffbf9ec500cf09708bf92bea15abac018ad4abe
Andres Alejos <andres.alejos@gmail.com>
2023-05-01 23:30:09 -0400

    dmatrix constructor w/ dense and csr

--- lib/exgboost.ex
+++ lib/exgboost.ex
@@ -1,7 +1,10 @@
 defmodule Exgboost do
+  alias Exgboost.DMatrix
   import Exgboost.Internal
 
-  def dmatrix(%Nx.Tensor{} = tensor, opts \\ []) do
+  def dmatrix(value, opts \\ [])
+
+  def dmatrix(%Nx.Tensor{} = tensor, opts) do
     opts =
       Keyword.validate!(opts, [
         :label,
@@ -14,24 +17,70 @@ defmodule Exgboost do
         config: %{missing: -1.0}
       ])
 
-    IO.inspect(opts)
-
     {config, opts} = Keyword.pop!(opts, :config)
-
-    array_interface = array_interface(tensor)
 
     dmat =
       Exgboost.NIF.dmatrix_create_from_dense(
-        Jason.encode!(array_interface),
+        Jason.encode!(array_interface(tensor)),
         Jason.encode!(config)
       )
       |> unwrap!()
 
+    dmatrix(%DMatrix{ref: dmat}, opts)
+  end
+
+  def dmatrix(%DMatrix{} = dmat, opts) do
+    opts =
+      Keyword.validate!(opts, [
+        :label,
+        :weight,
+        :base_margin,
+        :group,
+        :label_upper_bound,
+        :label_lower_bound,
+        :feature_weights
+      ])
+
     Enum.each(opts, fn {key, value} ->
       data_interface = array_interface(value) |> Jason.encode!()
-      Exgboost.NIF.dmatrix_set_info_from_interface(dmat, Atom.to_string(key), data_interface)
+      Exgboost.NIF.dmatrix_set_info_from_interface(dmat.ref, Atom.to_string(key), data_interface)
     end)
 
-    %Exgboost.DMatrix{ref: dmat}
+    dmat
+  end
+
+  def dmatrix(
+        %Nx.Tensor{} = indptr,
+        %Nx.Tensor{} = indices,
+        %Nx.Tensor{} = data,
+        ncol,
+        opts \\ []
+      )
+      when is_integer(ncol) do
+    opts =
+      Keyword.validate!(opts, [
+        :label,
+        :weight,
+        :base_margin,
+        :group,
+        :label_upper_bound,
+        :label_lower_bound,
+        :feature_weights,
+        config: %{missing: -1.0}
+      ])
+
+    {config, opts} = Keyword.pop!(opts, :config)
+
+    dmat =
+      Exgboost.NIF.dmatrix_create_from_csr(
+        Jason.encode!(array_interface(indptr)),
+        Jason.encode!(array_interface(indices)),
+        Jason.encode!(array_interface(data)),
+        ncol,
+        Jason.encode!(config)
+      )
+      |> unwrap!()
+
+    dmatrix(%DMatrix{ref: dmat}, opts)
   end
 end
