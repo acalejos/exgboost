86e12b9b51ca9c7bea1a12020cdf6601644dfe79
acalejos <andres.alejos@gmail.com>
2024-01-27 12:29:40 -0500

    Update livebook and Mix env

--- lib/exgboost/plotting/styles.ex
+++ lib/exgboost/plotting/styles.ex
@@ -1,25 +1,19 @@
-defmodule EXGBoost.Plotting.Styles do
-  @bst File.cwd!() |> Path.join("test/data/model.json") |> EXGBoost.read_model()
+Mix.env() == :docs &&
+  defmodule EXGBoost.Plotting.Styles do
+    @bst File.cwd!() |> Path.join("test/data/model.json") |> EXGBoost.read_model()
 
-  @moduledoc """
-  <div class="vega-container">
-  #{Enum.map(EXGBoost.Plotting.get_styles(), fn {name, _style} -> """
-    <div class="vega-item">
-      <h2>#{name}</h2>
-      <pre>
-        <code class="vega-lite">
-        #{EXGBoost.plot_tree(@bst, style: name, height: 200, width: 300).spec |> Jason.encode!()}
-        </code>
-      </pre>
+    @moduledoc """
+    <div class="vega-container">
+    #{Enum.map(EXGBoost.Plotting.get_styles(), fn {name, _style} -> """
+      <div class="vega-item">
+        <h2>#{name}</h2>
+        <pre>
+          <code class="vega-lite">
+          #{EXGBoost.plot_tree(@bst, style: name, height: 200, width: 300).spec |> Jason.encode!()}
+          </code>
+        </pre>
+      </div>
+      """ end) |> Enum.join("\n\n")}
     </div>
-    """ end) |> Enum.join("\n\n")}
-  </div>
-  """
-
-  @functions [{:a, "a"}, {:b, "b"}, {:c, "c"}]
-  for {name, ret} <- @functions do
-    def unquote(name)() do
-      unquote(ret)
-    end
+    """
   end
-end
